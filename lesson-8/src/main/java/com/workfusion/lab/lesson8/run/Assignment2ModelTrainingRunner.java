b604d91963accb86142237a415b322fd0f7a2780
Ruslan Dubovik <rdubovik@workfusion.com>
2018-08-01 11:43:25 +0300

    lessons8-delete empty lines

--- lesson-8/src/main/java/com/workfusion/lab/lesson8/run/Assignment2ModelTrainingRunner.java
+++ lesson-8/src/main/java/com/workfusion/lab/lesson8/run/Assignment2ModelTrainingRunner.java
@@ -27,6 +27,7 @@ public class Assignment2ModelTrainingRunner {
      * Input directory path to use.
      */
     public final static String INPUT_DIR_PATH = "data/train";
+
     /**
      * Output directory path to use.
      */
